2688cef975dc50b2262ef9a7c225b2c7dcb07af3
ygcoutinho <yagogc.it@gmail.com>
2021-12-28 20:40:54 -0300

    criação da botton navigation bar e ajustes na app bar

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -2,6 +2,7 @@ import 'package:app_folio/components/AppBar/appbar_content.dart';
 import 'package:app_folio/components/card/card_widget.dart';
 import 'package:app_folio/data/dummy_data.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -10,19 +11,46 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const PreferredSize(
-        preferredSize: Size.fromHeight(130),
+        preferredSize: Size.fromHeight(100),
         child: AppBarContent(),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(14.0),
+        padding: const EdgeInsets.only(left: 7, right: 7),
         child: ListView.builder(
-            itemCount: DUMMY_DATA.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.only(bottom: 10),
-                child: CardWidget(cardItems: DUMMY_DATA[index]),
-              );
-            }),
+          itemCount: DUMMY_DATA.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: CardWidget(cardItems: DUMMY_DATA[index]),
+            );
+          },
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: const Color(0xFFFFFFFF),
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Atividades',
+            icon: FaIcon(
+              FontAwesomeIcons.bullseye,
+              size: 24,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'Repositórios',
+            icon: FaIcon(
+              FontAwesomeIcons.github,
+              size: 24,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'Sobre o dev',
+            icon: FaIcon(
+              FontAwesomeIcons.userAlt,
+              size: 24,
+            ),
+          ),
+        ],
       ),
     );
   }
